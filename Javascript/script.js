9dd164590edc98a423e9aed957efec9cc2ab26bb
Federico Silvera <federiconicolas007@gmail.com>
2022-09-01 09:54:55 -0300

    siguiendo con los problemas xd

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,8 +1,4 @@
-function calcular_dolar( pesos ){
-    let dolar = pesos / 43
-    console.log ( dolar);
-    
-}
+
 // let lista_monetaria = [{moneda:"Dolar", valor:43}, {moneda:"Euro", valor:38}, {moneda:"Argentinos", valor:"00,5"}, {moneda:"Real", valor:"6,90"}, {moneda:"Uruguayos", valor:1}];
 // console.log("Cotizacion monetaria", lista_monetaria);
 // for(let i=0; i<5; i++){
@@ -43,19 +39,24 @@ function calcular_dolar( pesos ){
 // calcular_dolar(pesos_uyu);
 
 // }
-
+function calcular_dolar( pesos ){
+    let dolar = pesos / 43
+    console.log ( dolar);
+    
+}
 
 let boton = document.getElementById("boton");
 
 boton.addEventListener("click", function(){
-    let monto = document.getElementById("monto");
+    let monto =document.getElementById("monto");
     let lista = document.getElementById("lista");
     
+   
 
     let li = document.createElement("li");
-
-    li.innerHTML = `<span>${monto.value}</span>`
-
+    
+    li.innerHTML = `<span>${calcular_dolar(monto.value)}</span>`
+    
     lista.append(li);
     event.preventDefault()
     
